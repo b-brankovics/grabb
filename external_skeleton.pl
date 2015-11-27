adedb2737e2fe58b1868949be899cd96a6fd5caf
b-brankovics <brankovics.balazs@gmail.com>
2015-11-27 16:55:23 +0100

    extended for mapping-assemblers

--- external_skeleton.pl
+++ external_skeleton.pl
@@ -13,6 +13,10 @@ my @param2 = split / /, $parameters2;
 # If there were no extra arguments passed for the assembler at the invocation of GRAbB
 #   then the value passed to this script is "-" ($param1[0] and/or $param2[0])
 
+# For mapping-assemblers a reference is needed
+#my $ref = &get_reference();  # For reference based assembly uncomment this line
+
+
 # The program to be used for assembly
 my $assmebler = ""; # ADD
 
@@ -24,3 +28,13 @@ my $result = ""; # ADD
 
 # Copy the result file to the expected position
 `perl -ne 'if (/^>/) {print} else {tr/acgtwmrskybvdh/ACGTWMRSKYBVDH/; print}' $result >$outfile`;
+
+sub get_reference {
+    # This subroutine finds the reference file for the current thread
+    my $ref;
+    my $pwd = $ENV{"PWD"};
+    $pwd =~ /(thread_\d+)$/;
+    my $thread = $1;
+    $ref = "../../$thread/bait.fas";
+    return $ref;
+}
